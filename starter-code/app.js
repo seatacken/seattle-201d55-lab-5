ce1a287241e8b2f627eee2d371adcfc1b76a6316
seatacken <57050116+seatacken@users.noreply.github.com>
2019-12-06 18:23:52 -0800

    finish problem 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -83,13 +83,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
-
+function sumArray() { //eslint-disable-line
+var newArray = [];
+var sumAnswer = sum(testArray[0],testArray[1])[0];
+var finalSum = sum(sumAnswer,testArray[2])[0];
+newArray[0] = finalSum;
+newArray[1] = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + newArray[0] + ' is their sum.'
+return newArray;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
