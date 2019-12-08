cb3ab72abb831631712737a42d1401cdd38e1e33
seatacken <57050116+seatacken@users.noreply.github.com>
2019-12-08 15:53:44 -0800

    finish lab 5 with attempt at stretch goal

--- starter-code/app.js
+++ starter-code/app.js
@@ -141,12 +141,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
-
-function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+function multiplyAnyArray() { //eslint-disable-line
+  var bonusNewArray = []
+  for (var i = 0; i < testDynamicArray.length; i++){
+    var bonusProductAnswer = multiply(testDynamicArray[i],testDynamicArray[i])[0];
+    var bonusFinalProduct = multiply(bonusProductAnswer,testDynamicArray[i])[0];
+  }
+  bonusNewArray[0] = bonusFinalProduct;
+  bonusNewArray[1] = 'The numbers ' + testDynamicArray + ' have a product of ' + bonusFinalProduct;
+  return bonusNewArray;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
